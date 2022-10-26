2381f5723d64040748ba3deb94a63bb6de80b869
xma12 <xma12@slb.com>
2022-10-26 13:49:02 +0800

    test: create external expense other expense happy test

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -50,8 +50,13 @@ namespace Expense.Service.Test
         public void Should_return_other_expense_type_if_project_is_external_and_has_other_name()
         {
             // given
+            Project project = new Project(ProjectType.EXTERNAL, "Project C");
+
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
+
             // then
+            Assert.Equal(ExpenseType.OTHER_EXPENSE, expenseType);
         }
 
         [Fact]
